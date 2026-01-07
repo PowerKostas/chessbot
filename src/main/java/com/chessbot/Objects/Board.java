26be2571e88f29dd29a22f41906a4e26e1ff9a02
PowerKostas <konstantinosvrousgos@gmail.com>
2026-01-08 01:28:54 +0200

    Reformatting completed

--- src/main/java/com/chessbot/Objects/Board.java
+++ src/main/java/com/chessbot/Objects/Board.java
@@ -2,8 +2,11 @@ package com.chessbot.Objects;
 
 import com.chessbot.BoardUtils.DragMove;
 import com.chessbot.BoardUtils.FenReader;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
 
+// Board class that is used to represent the board in JavaFX and the board state
 public class Board extends GridPane {
     // 64 bit variable that each bit represent a piece on the board, used for board representation
     private long allPiecesBitboard;
@@ -11,8 +14,23 @@ public class Board extends GridPane {
     private final DragMove dragMove = new DragMove();
 
 
-    // Prepares every custom square class for the JavaFX board
     public Board() {
+        // Sets up FXML
+        this.setPrefSize(700, 700);
+
+        for (int i = 0; i < 8; i++) {
+            ColumnConstraints colConstraint = new ColumnConstraints();
+            colConstraint.setPercentWidth(12.5);
+            this.getColumnConstraints().add(colConstraint);
+        }
+
+        for (int i = 0; i < 8; i++) {
+            RowConstraints rowConstraint = new RowConstraints();
+            rowConstraint.setPercentHeight(12.5);
+            this.getRowConstraints().add(rowConstraint);
+        }
+
+        // Prepares every custom square class for the JavaFX board
         for (int row = 0; row < 8; row += 1) {
             for (int col = 0; col < 8; col += 1) {
                 Square square = new Square(row, col, dragMove);
@@ -38,14 +56,10 @@ public class Board extends GridPane {
             long mask = 1L << i;
             if ((allPiecesBitboard & mask) != 0) {
                 square.setStyle("-fx-background-color: red");
-            }
-
-            else {
+            } else {
                 if ((square.getRow() + square.getCol()) % 2 == 0) {
                     square.setStyle("-fx-background-color: #ebecd0");
-                }
-
-                else {
+                } else {
                     square.setStyle("-fx-background-color: #739552");
                 }
             }
--- src/main/java/com/chessbot/ViewManager.java
+++ src/main/java/com/chessbot/ViewManager.java
@@ -2,18 +2,23 @@ package com.chessbot;
 
 import com.chessbot.Objects.Board;
 import javafx.fxml.FXML;
+import javafx.scene.layout.HBox;
 
 public class ViewManager {
     @FXML
-    private Board boardOneController;
+    private HBox mainContainer;
 
-    @FXML
-    private Board boardTwoController;
+    private Board boardOne;
+    private Board boardTwo;
 
 
     public void initialize() {
-        boardOneController.setBoard("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR");
-        boardTwoController.setBoard("8/8/8/8/8/8/8/8");
-        boardTwoController.bitboardVisualization(boardOneController.getAllPiecesBitboard());
+        boardOne = new Board();
+        boardTwo = new Board();
+        mainContainer.getChildren().addAll(boardOne, boardTwo);
+
+        boardOne.setBoard("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR");
+        boardTwo.setBoard("8/8/8/8/8/8/8/8");
+        boardTwo.bitboardVisualization(boardOne.getAllPiecesBitboard());
     }
 }
